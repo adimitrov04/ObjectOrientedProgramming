1143c21980db84ee8d95b920f0e0d4095712f331
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-02-20 08:18:44 +0200

    [01Structs] Task 02: Improve format validation

--- Pracicum/01Structs/02Distance.cpp
+++ Pracicum/01Structs/02Distance.cpp
@@ -13,27 +13,29 @@ void prints (Distance dist)
     std::cout << dist.feet << '\'' << dist.inches << '\"' << endl;;
 }
 
+void makeValidFormat (Distance& invalid)
+{
+    const int INCHES_IN_FEET = 12;
+
+    invalid.feet += (invalid.inches / INCHES_IN_FEET);
+    invalid.inches -= (invalid.inches - (invalid.inches % INCHES_IN_FEET));
+}
+
 Distance adds (Distance const& dist1, Distance const& dist2)
 {
-    const int inchesInFeet = 12;
-    
     Distance totalDist;
     totalDist.feet = dist1.feet + dist2.feet;
     totalDist.inches = dist1.inches + dist2.inches;
 
-    while (totalDist.inches >= inchesInFeet)
-    {
-        totalDist.feet++;
-        totalDist.inches -= inchesInFeet;
-    }
+    makeValidFormat(totalDist);
 
     return totalDist;
 }
 
 int main ()
 {
-    Distance throwDist = {10, 5};
-    Distance rollDist = {1, 7};
+    Distance throwDist = {0, 49};
+    Distance rollDist = {0, 11};
 
     Distance totalDist = adds(throwDist, rollDist);
     prints(totalDist);
