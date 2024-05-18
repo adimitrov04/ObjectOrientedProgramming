fc880290832a3937e9e989f652c6c8c4474a0e93
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-05-18 16:20:05 +0300

    Implement Vector copy constructor, operators and copy method; add bug fixes and safeguards to previous methods

--- SocialMedia/include/Containers/Vector.h
+++ SocialMedia/include/Containers/Vector.h
@@ -15,14 +15,47 @@ public:
     : f_capacity(0), f_size(0), arr(nullptr)
     {}
 
-    Vector (const Vector& other) = delete;
+    Vector (const size_t startingCapacity)
+    : Vector<T>()
+    {
+        reserve(startingCapacity);
+    }
 
-    Vector<T>& operator= (const Vector& other) = delete;
-    T& operator[] (const size_t index) = delete;
-    const bool operator== (const Vector& other) const = delete;
+    Vector (const Vector<T> &other)
+    : Vector<T>()
+    {
+        copy(other);
+    }
 
-    std::ostream& operator<< (std::ostream& out) const = delete;
-    std::istream& operator>> (std::istream& in) const = delete;
+    Vector<T>& operator= (const Vector& other)
+    {
+        if (this != &other)
+            copy(other);
+
+        return *this;
+    }
+    
+    T& operator[] (const size_t index) const
+    {
+        return arr[index];
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const Vector<T> &vec)
+    {
+        vec.print(out);
+
+        return out;
+    }
+
+    friend std::istream& operator>> (std::istream& in, Vector<T> &vec)
+    {
+        size_t count = 0;
+        in >> count;
+
+        vec.read(count, in);
+
+        return in;
+    }
 
     virtual ~Vector ()
     {
@@ -52,6 +85,8 @@ public:
     {
         for (int i = 0; i < size(); i++)
             out << arr[i] << ' ';
+
+        out << '\n';
     }
     
     virtual void push_back (const T& element)
@@ -82,9 +117,15 @@ public:
 
     void reserve (const size_t capacity)
     {
+        if (capacity == 0)
+            clear();
+
+        if (capacity <= this->capacity())
+            return;
+        
         T* buffer = new T[capacity];
 
-        for (int i = 0; i < size(); i++)
+        for (size_t i = 0; i < size(); i++)
         {
             buffer[i] = arr[i];
         }
@@ -96,15 +137,15 @@ public:
         f_capacity = capacity;
     }
 
-    virtual void read (const size_t size, std::istream& in = std::cin)
+    virtual void read (const size_t count, std::istream& in = std::cin)
     {
         if (this->size() > 0)
             clear();
 
-        if (size > capacity())
-            reserve(size);
+        if (count > capacity())
+            reserve(count);
 
-        for (int i = 0; i < size; i++)
+        for (size_t i = 0; i < count; i++)
         {
             T addNew;
             in >> addNew;
@@ -123,7 +164,17 @@ public:
     }
 
 protected:
-    void copy (const Vector<T> &other);
+    // This method leaves vector empty if it cannot reserve the necessary memory for copy. Fix later.
+    virtual void copy (const Vector<T> &other)
+    {
+        clear();
+        reserve(other.capacity());
+
+        for (size_t i = 0; i < other.size(); i++)
+        {
+            push_back(other[i]);
+        }
+    }
 
 protected:
     size_t f_capacity;
